85f658c07c8c2eb8dcd8f527daff0bc24d10f18c
Bobby Davis <bdavis@core-techs.net>
2017-03-01 16:56:10 -0500

    Add project files.

---
+++ reverseAString/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using reverseAString.Models;
+
+namespace reverseAString.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Reverse mReverse = new Reverse { sInput = "", sOutput = "" };
+            return View(mReverse);
+        }
+
+     
+        [HttpPost]
+        public ActionResult Index(string sInput, string sOutput)
+        {
+            Reverse mReverse = new Reverse { sInput = "", sOutput = "" };
+
+            mReverse.sInput = sInput;
+
+            char[] cArray = sInput.ToCharArray();
+            Array.Reverse(cArray);
+
+            mReverse.sOutput = new string(cArray);
+            return View(mReverse);
+        }
+
+        
+    }
+}
---
+++ reverseAString/Models/Reverse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace reverseAString.Models
+{
+    public class Reverse
+    {
+        [AllowHtml]
+        public string sInput { get; set; }
+
+        [AllowHtml]
+        public string sOutput { get; set; }
+    }
+}
